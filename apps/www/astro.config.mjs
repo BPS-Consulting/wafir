60aed26583fd67e131504d973ccf1ec9b14ab731
Alvin Mei <alvinkmei@gmail.com>
2026-01-15 12:41:13 -0500

    workflows

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bps-consulting.github.io",
+  base: "/wafir",
   vite: {
     plugins: [tailwindcss()],
   },
