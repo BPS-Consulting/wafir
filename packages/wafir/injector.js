dc9f90774c2be7bdfc4054798c40d2c75de584c7
Alvin Mei <alvinkmei@gmail.com>
2026-04-08 14:16:51 -0400

    added options to injector

--- packages/wafir/injector.js
+++ packages/wafir/injector.js
@@ -3,6 +3,9 @@
   const SCRIPT_URL =
     "https://wafir-all.s3.us-east-2.amazonaws.com/wafir/latest/wafir.js";
 
+  const BRIDGE_URL = ""; // Blank for default
+  const CONFIG_URL = "https://your-config-url.com";
+
   if (document.querySelector(TAG_NAME)) {
     console.log("Widget already loaded.");
     return;
@@ -10,10 +13,15 @@
 
   const script = document.createElement("script");
   script.type = "module";
+  script.crossOrigin = "anonymous";
   script.src = SCRIPT_URL;
 
   script.onload = () => {
     const widget = document.createElement(TAG_NAME);
+
+    // Set the attributes for the Lit component
+    if (BRIDGE_URL) widget.setAttribute("bridge-url", BRIDGE_URL);
+    if (CONFIG_URL) widget.setAttribute("config-url", CONFIG_URL);
 
     document.body.appendChild(widget);
   };
